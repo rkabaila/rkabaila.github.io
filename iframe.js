7118457e41dda673c17c8e639b59ca7fb5dfb78c
Rimantas Kabaila <rimas.kabaila@gmail.com>
2023-07-31 18:18:50 +0300

    test

--- iframe.js
+++
@@ -1,5 +0,0 @@
-export const renderComponent = (div) => {
-  const iframe = document.createElement("iframe");
-  iframe.src = "https://www.youtube.com/embed/XHQBiozafb0";
-  div.appendChild(iframe);
-};
